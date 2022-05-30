9066cacf36a56ecff2fb1da57f624bb06cb271a3
Jacob Kang <jacob@litbig.com>
2022-05-30 16:55:19 +0900

    slack parsing map으로 변경

--- src/functions/menu/handler.ts
+++ src/functions/menu/handler.ts
@@ -15,23 +15,27 @@ const tomorrow = ['tomorrow', '내일']
 const connection = createConnection(connectionOptions)
 
 const handler: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
-    let when: string
     const now = new Date(); // 현재 시간
     const utcNow = now.getTime() + (now.getTimezoneOffset() * 60 * 1000); // 현재 시간을 utc로 변환한 밀리세컨드값
     const koreaTimeDiff = 9 * 60 * 60 * 1000; // 한국 시간은 UTC보다 9시간 빠름(9시간의 밀리세컨드 표현)
     let date = new Date(utcNow + koreaTimeDiff); // utc로 변환된 값을 한국 시간으로 변환시키기 위해 9시간(밀리세컨드)를 더함
+    let bodyMap = new Map<string, string>()
+    let when: string
 
     try {
         if (event.body.hasOwnProperty('text')) {
             when = event.body.text
         } else if (event.hasOwnProperty('body')) {
-            let what = `${event.body}`
-            console.log(what.split('text')[1].split('&')[0])
-            when = what.split('text')[1].split('&')[0]
+            `${event.body}`.split('&').forEach(value => {
+                let keyValue = value.split('=')
+                bodyMap.set(keyValue[0], decodeURI(keyValue[1]))
+            })
+            if (bodyMap.has('text')) {
+                when = bodyMap.get('text')
+            } else {
+                when = null
+            }
         }
-        when = when.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gim, '')
-
-        console.log(`when: ${when}`)
 
         today.forEach(it => {
             if (when.includes(it)) {
@@ -48,6 +52,8 @@ const handler: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event)
             }
         })
 
+        when = when.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/^\s+$]/gim, '')
+
         if (when.length == 0) {
             when = 'today'
         }
@@ -58,7 +64,6 @@ const handler: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event)
 
     console.log(`when: ${when}, date: ${getDbDate(date)}`)
 
-    
     const repository = (await connection).getCustomRepository(MenuRepository)
     const menu = await repository.getMenuByDate(getDbDate(date))
     console.log(menu)
