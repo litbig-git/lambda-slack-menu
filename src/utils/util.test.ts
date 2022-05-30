3043dccf3d10524ab6708c2f145b1e36f8cd1680
Jacob Kang <jacob@litbig.com>
2022-05-30 17:51:18 +0900

    util test 추가

--- src/utils/util.test.ts
+++ src/utils/util.test.ts
@@ -1,4 +1,4 @@
-import { getDbDate, getDisplayDate, toComma } from "./util";
+import { getDbDate, getDisplayDate, toComma, wordReplace } from "./util";
 
 test('toComma', () => {
   expect(toComma('닭곰탕\n쌀밥/잡곡밥/누룽지\n베이컨감자볶음\n도토리묵야채무침\n깍두기\n셀프토스트&커피&시리얼&우유'))
@@ -13,4 +13,9 @@ test('getDbDate', () => {
 test('getDisplayDate', () => {
   expect(getDisplayDate(new Date('2022-05-27')))
     .toBe('May 27, 2022')
+})
+
+test('wordReplace', () => {
+  expect(wordReplace('tomorrow breakfast'))
+    .toStrictEqual(['breakfast', true])
 })
